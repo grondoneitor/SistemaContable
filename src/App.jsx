044d2ccaf9ee2af3051565f18b39e7fe5838f01a
Facundo Grondona <facundo.grondona.02@gmail.com>
2025-01-09 16:45:45 -0300

    Optimizo el codigo de Home.jsx pasandole un array con los menus  sus datos por prop y iterando en el

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,22 @@ import FormCrearCategoria from './components/Categorias/FormCrearCategoria.jsx'
 import FormDetalleCategoria from './components/Categorias/FormDetalleCategoria.jsx'
 function App() {
 
+  const array = [
+    {url: "/clientes", nombre:"Clientes"},
+    {url:"/productos",nombre:"Productos"},
+    {url:"/",nombre:"Ventas"},
+    {url:"/", nombre:"Compras y proveedores"},
+    {url:"/", nombre:"Estado de cuenta"}
+  ]
 
   return (
     <body>
       <Routes>
-        <Route path="/" element={<Home />} >
+        <Route path="/" element={
+          <Home
+            array={array}
+            />
+          } >
           <Route path="clientes" element={<h1>el cliente</h1>} />
           <Route path='productos' element={<ShowProductos />} >
             <Route path='categorias' element={<Categorias />} >
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,148 +1,56 @@
 import { Link, Outlet } from "react-router-dom";
 
-export default function Home() {
+// eslint-disable-next-line react/prop-types
+export default function Home({ array }) {
+  const algo = array
   return (
     <body className="w-full">
       <h1 className="text-center my-9 text-5xl text-slate-800 font-semibold">Llevemos el control de tu pagina</h1>
       <h2 className="text-center text-3xl text-slate-700 font-bold ">Nombre empresa</h2>
 
-       
-       <main className="flex w-full   h-screen ">
-       <nav className=" ml-2 pl-4  rounded-md bg-white w-72 h-full border-r-2 border-b-2 border-slate-800 ">
-        <div className="flex justify-center items-center h-16">
-          <div className="w-2/3 bg-black h-1"></div>
-        </div>
-        <ul className="grid  gap-3 justify-around mb-6">
-          <li className="  shadow-2xl    border-2 border-slate-50 items-center gap-2  ">
-            <Link to="/clientes" className=" flex items-center gap-2 text-slate-50 ">
 
-              <svg
-                className="w-6   text-slate-800"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-              >
+      <main className="flex w-full   h-screen ">
+        <nav className=" ml-2 pl-4  rounded-md bg-white w-72 h-full border-r-2 border-b-2 border-slate-800 ">
+          <div className="flex justify-center items-center h-16">
+            <div className="w-2/3 bg-black h-1"></div>
+          </div>
+          <ul className="grid  gap-3 justify-around mb-6">
+            {
+              algo.map((item) => {
+                return (
+                  <li key={item.url} className="  shadow-2xl    border-2 border-slate-50 items-center gap-2  ">
+                  <Link to={item.url} className=" flex items-center gap-2 text-slate-50 ">
 
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
-                />
-              </svg>
-              <p className="text-xl text-slate-800">Clientes</p>
-            </Link>
-          </li>
+                    <svg
+                      className="w-6   text-slate-800"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                    >
 
-          <li className="  shadow-2xl   border-2 border-slate-50  items-center gap-2">
-            <Link to="/productos" className=" flex items-center gap-2 text-slate-50 ">
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                      />
+                    </svg>
+                    <p className="text-xl text-slate-800">{item.nombre}</p>
+                  </Link>
+                </li>
+                )
+  
+              })
+            }
+          </ul>
+        </nav>
 
-              <svg
-                className="w-6 text-slate-800"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"
-                />
-              </svg>
-              <p className="text-xl text-slate-800">Productos</p>
-            </Link>
-          </li>
-          <li className="  shadow-2xl   border-2 border-slate-50  items-center gap-2">
-            <Link to="/categorias" className=" flex items-center gap-2 text-slate-50 ">
-
-              <svg
-                className="w-6 text-slate-800"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"
-                />
-              </svg>
-              <p className="text-xl text-slate-800">Categorias</p>
-            </Link>
-          </li>
-          <li className="  shadow-2xl   border-2 border-slate-50  items-center gap-2">
-            <Link to="/" className="flex items-center gap-2 text-slate-50 ">
-              <svg
-                className="w-6 text-slate-800"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-              </svg>
-              <p className="text-xl text-slate-800">Ventas</p>
-
-            </Link>
-          </li>
-
-          <li className=" shadow-2xl   border-2 border-slate-50  items-center gap-2">
-            <Link to="/" className=" flex items-center gap-2 text-slate-50 ">
-              <svg
-                className="w-6 text-slate-800"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-                />
-              </svg>
-              <p className="text-xl text-slate-800">Compras y proveedores</p>
-
-            </Link>
-          </li>
-
-          <li className="   shadow-2xl   border-2 border-slate-50  items-center gap-2">
-            <Link to="/" className=" flex items-center gap-2 text-slate-50 ">
-              <svg
-                className="w-6 text-slate-800"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
-                />
-              </svg>
-              <p className="text-xl text-slate-800">Estado de cuenta</p>
-
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <section className="mt-4 w-full flex flex-col items-center">
-        <Outlet />
-      </section>
-       </main>
+        <section className="mt-4 w-full flex flex-col items-center">
+          <Outlet />
+        </section>
+     
+      </main>
 
     </body>
   );
